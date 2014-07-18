137821cb2579f7026c2996ca6286cd471caaf509
Amanda <amand00@hitachi-id.com>
2014-07-18 17:02:25 -0600

    first commit

--- books/settings.py
+++ books/settings.py
@@ -87,9 +87,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "static/"
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+    
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
